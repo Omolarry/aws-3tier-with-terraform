48631487418d38710faf7d169db62f76bfc0306e
Omolarry <tomikefamoroti@gmail.com>
2023-04-19 21:22:14 +0100

    second commit

--- my-website-ecs/backend.tf
+++ my-website-ecs/backend.tf
@@ -1,8 +1,8 @@
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket    = "Jupiter-terraform-remote-state"
-    key       = "jupiter-website-ecs.tfstate"
+    bucket    = "mywebsite-terraform-remote-state"
+    key       = "mywebsite-website-ecs.tfstate"
     region    = "us-east-1"
     profile   = "default"
   }
